bdcb462edd8224388965656a16bd818b513d3b75
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-05-13 09:53:14 -0700

    adding coalesce sample

---
+++ 10-coalesce-parameter/MyProject.Con.CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyProject.Con
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {// 1234
+            Console.WriteLine("Hello World 2015!");
+        }
+    }
+}

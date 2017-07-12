4e0fe50c140ceae48de873261345e4e0423b53e0
Sayed Ibrahim Hashimi <sayed.hashimi@gmail.com>
2017-07-12 13:30:49 -0700

    adding comment sample, wip

---
+++ 16-conditional-comments/MyProject.Con/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace MyProject.Con
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(@"Hello world");
+
+            string title = "AppTitle";
+////#if (DisplayTitle)
+// Console.WriteLine($"Title: {title}");
+////#endif
+
+////#if (DisplayTitle)
+////    Actual Comment
+//    content
+//#endif
+// end comment
+//// end quad comment
+        }
+    }
+}
